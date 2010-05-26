90c8163276cac6dcff7adc97fafe3d1bc03a584f
Rich Chang <emailrchang@gmail.com>
2010-05-26 16:24:24 -0400

    - create new unit test bundle that enables debugging of unit tests.

---
+++ Tests/FirstTest.m
@@ -0,0 +1,22 @@
+//
+//  FirstTest.m
+//  ulticast
+//
+//  Created by Richard Chang on 5/26/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "FirstTest.h"
+
+
+@implementation FirstTest
+
+- (void) testMath {
+    
+    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
+    
+}
+
+
+
+@end
